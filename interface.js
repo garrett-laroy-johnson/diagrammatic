6fb6f9d81d24cefae266723b41612836b9111745
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2021-11-10 17:09:34 -0600

    functions to objects
    added display  functions to MediaBundle object. should scale better.

--- interface.js
+++ interface.js
@@ -4,6 +4,10 @@ let mediaBundles = [];
 let width = window.innerWidth;
 let height = window.innerHeight;
 
+//HTML edit editor interface template reference
+const mbEdit = document.getElementById("mbModel"); // creates interface editor
+const  main = document.getElementById("main");
+console.log(main);
 //
 // let dropbox;
 //
@@ -19,16 +23,51 @@ let MediaBundle = class {
     this.y = y;
     this.index = index;
     this.marker;
+    this.objects = [];
+    this.ui = {};
+    this.filelist = [];
     //this.editing = false;
+    }
+
+  createUI() {
+    this.ui = {
+          // set IDs for HTML DOM elements that need JS interaction
+          module : this.name+"_module",
+          trash : this.name+"_trash",
+          input : this.name+"_input",
+      }
+       console.log(this.ui.module);
+        let clone = mbEdit.content.cloneNode(true);
+        clone.querySelector("div").id = this.ui.module;
+        clone.querySelector("svg").id = this.ui.trash;
+        clone.querySelector("svg").setAttribute("onclick", "deleteMB(" + this.ui.module + ")");
+        clone.querySelector("h3").textContent = this.name;
+        clone.querySelector("input").id = this.ui.input;
+        clone.getElementById(this.ui.input).addEventListener("change", this.handleFile, false);
+          main.appendChild(clone);
+
+  }
+
+  handleFile() {
+    //console.log(file);
+  //  console.log(file.data)
+    this.filelist = this.files; /* now you can work with the file list */
+    console.log(this.filelist)
+    }
+
+  createObject(name, origin, path) {
+    let b = new MediaObject(name);
+    this.objects.push(b);
   }
   get position() {
     return (this.x, this.y);
   }
+
 }
 
 
 let MediaObject = class {
-  constructor(name, index) {
+  constructor(name, path) {
     this.name = name;
     this.path = path;
   }
@@ -41,62 +80,17 @@ const editor = {
 function editing(id){
 
   if (editor.editing != "none"){
+    document.getElementById(editor.editing).classList.add('mb');
+    document.getElementById(editor.editing).classList.remove('mb-active');
+    editor.editing = id;
+    document.getElementById(editor.editing).classList.add('mb-active');
+    document.getElementById(editor.editing).classList.remove('mb');
+  } else {
+    editor.editing = id;
+    document.getElementById(editor.editing).classList.add('mb-active');
+    document.getElementById(editor.editing).classList.remove('mb');
+} }
 
-  document.getElementById(editor.editing).classList.add('mb');
-
-  document.getElementById(editor.editing).classList.remove('mb-active');
-
-  editor.editing = id;
-
-  document.getElementById(editor.editing).classList.add('mb-active');
-
-  document.getElementById(editor.editing).classList.remove('mb');
-
-  }
-
-  else {
-
-  console.log(id);
-
-  editor.editing = id;
-
-  console.log(editor.editing);
-
-  document.getElementById(editor.editing).classList.add('mb-active');
-
-  document.getElementById(editor.editing).classList.remove('mb');
-
-}
-
-}
-
-// function dragenter(e) {
-//   e.stopPropagation();
-//   e.preventDefault();
-// }
-//
-// function dragover(e) {
-//   e.stopPropagation();
-//   e.preventDefault();
-// }
-//
-// function drop(e) {
-//   e.stopPropagation();
-//   e.preventDefault();
-//
-//   const dt = e.dataTransfer;
-//   const files = dt.files;
-//
-//   handleFiles(files);
-// }
-
-//function draw() {
-
-//for (let i = 0; i < mediaBundles.length; i++) {
-//  mediaBundles[i].write();
-
-//  }
-//}
 
 function writeDiagram(){
    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(mediaBundles));
@@ -107,8 +101,8 @@ function writeDiagram(){
    downloadAnchorNode.click();
    downloadAnchorNode.remove();
  }
-
- function readDiagram (){ //this is broke. might be a good way to fix: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+//this is broke. might be a good way to fix: https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
+function readDiagram(){
    var input = document.createElement('input');
  input.type = 'file';
 
@@ -121,78 +115,79 @@ var data = JSON.parse(fs.readFileSync(file));
 mediaBundles = data;
     }
  input.click();
- }
-
-
-
+}
 
 function deleteMB(name){
-console.log(mediaBundles);
-name.remove();
-let index = mediaBundles.find(element => element === name);
-mediaBundles.splice(index,1)
-console.log(mediaBundles);
+  name.remove();
+  let index = mediaBundles.find(element => element === name);
+  mediaBundles.splice(index,1)
 }
 
-
-
-function createMB(name) { //if new MB name passes checkName, triggered and create new MediaBundle and trigger showMB
-
-
-      let b = new MediaBundle(name, mediaBundles.length, (width/2),(height/2)); //creates new JSON
-        mediaBundles.push(b); //adds name of JSON to list of bundles
-
-  //  console.log(mediaBundles);
-
-  }
-
-function showMB(name){
-  let editor = document.getElementById("mbModel"); // creates interface editor
-  let main  = document.getElementById("main");
-  let trashName = "trash-"
-
-  // Update something in the template DOM.
-  let clone = editor.content.cloneNode(true);
-//set div ID
-  let div = clone.querySelector("div");
-  div.id = name;
-// set text accordings
-  let h3 = clone.querySelector("h3");
-  h3.textContent = name;
-// setup trashcan to ID and to  trigger deleteMB()
-  let trash = clone.querySelector("svg");
-  trash.id = trashName.concat(name);
-  trash.setAttribute("onclick", "deleteMB(" + name + ")")
-  main.appendChild(clone);
-
-}
-
-
-
-
-function checkName (){ //check to see if name is a repeat, trigger alert if so, otherwise trigger createMB
+function createMB() {
+  //grab + process name
   let inputName = document.getElementById("newMBname").value; //grab the input from the form
   inputName = inputName.replace(/\s/g, "_"); //remove spaces, add underscore
-  if (mediaBundles.includes(inputName) === true) //if media bundle already contains
-  {
-      $("#MBCreateOutput").empty();
-      $("#MBCreateOutput").show();
-      document.getElementById("MBCreateOutput").innerHTML = "There is already a MediaBundle called " + inputName + ", please find a new name or edit that one.";
-      $("#MBCreateOutput").fadeOut(5000);
-      return true;
+  //check for duplicates
+  if (checkName(inputName, "mediaBundle")) {
+    logMB(inputName, "duplicate"); //creates notice for the log
+  } else {
+    let b = new MediaBundle(inputName, mediaBundles.length, (width/2),(height/2)); //creates new JSON
+    mediaBundles.push(b); //adds name of JSON to list of bundles
+    b.createUI();
+    //showMB(inputName);
+    logMB(b.name, "created"); //creates notice for the log
   }
-  else {
-  createMB(inputName);
-  showMB(inputName);
-  logMB(inputName); //creates notice for the log
 }
+
+//check to see if name is a repeat.
+  function checkName (name, type){
+    switch (type) {
+      case "mediaBundle":
+        return mediaBundles.some(e => e.name === name);
+        break;
+      case "mediaObject":
+        return false;
+        console.log("checkName function isn't set up to check media Object names as mediaObjects haven't been implimented yet. gives default false, allowing for duplicate.");
+        break;
+      }
 }
 
 
-function logMB(inputName) {
+
+function logMB(inputName, log)
+  { // logs feedback to psuedoconsole
+  let text;
+  switch (log) {
+    case "duplicate":
+      text = "There is already a MediaBundle called " + inputName + ", please find a new name or edit that one.";
+      break;
+    case "created":
+      text = "You have successfully created a MediaBundle called " + inputName + ".";
+      break;
+    }
   $("#MBCreateOutput").empty();
   $("#MBCreateOutput").show();
-  document.getElementById("MBCreateOutput").innerHTML = "You've created a MediaBundle called " + inputName;
+  document.getElementById("MBCreateOutput").innerHTML = text;
   $("#MBCreateOutput").fadeOut(5000);
-
 }
+
+// function mbIndex(name,type) {
+//   switch (type) {
+//     case "mediaBundle":
+//       for (i=0;i<mediaBundles.length;i++){
+//         if (mediaBundles[i].name===name){
+//           return i;
+//         }
+//       }
+//       break;
+//     case "mediaObject":
+//     for (i=0;i<mediaBundles.length;i++){
+//       for (d=0;d<mediaBundles[i].objects.length;d++){
+//       if (mediaBundles[i].objects[d]===name){
+//         return [i,d];
+//       }
+//     }
+//   }
+//       break;
+//     }
+// }
